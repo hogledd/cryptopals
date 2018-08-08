78c0103cb04e11d878aea1eb8ca12ff4af849af8
hogledd <hogledd>
2018-08-08 02:17:43 -0400

    Pseudo code for challenge 3

---
+++ s1c3.py
@@ -0,0 +1,27 @@
+"""
+@author: hogledd
+@date: 08/08/18
+
+Cryptopals Set 1 Challenge 3
+
+"""
+
+import click
+
+@click.command()
+@click.option('--ciphertext', prompt='ciphertext > ', help='The ciphertext you wish to denrypt.')
+
+def score_try:
+    # check frequency of letters in sample
+    # compare frequencies for common letters
+    # the closer (e.g. standard deviations) to well known english frequencies the better the score (e.g. 0-100)
+    # return score
+
+def decrypt(ciphertext):
+    print(ciphertext)
+    # attempt a key (i.e. brute force)
+    # go get a score
+    # show results for best scores
+
+if __name__ == '__main__':
+    decrypt()
